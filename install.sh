7d82013b879d093ecf9cd80f97e437d707bf5d8e
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 12:39:27 +0200

    init

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Backup
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+
+# Clone core
+git clone https://github.com/silvnvim/nvim ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+if [ $# -eq 0 ]
+then
+    git clone --recursive https://github.com/silvnvim/starter ~/.config/nvim/lua/config
+else
+    if [ "$1" = "starter" ]
+    then
+        git clone --recursive https://github.com/silvnvim/starter ~/.config/nvim/lua/config
+    elif [ "$1" = "none" ]
+    then
+        :
+    else
+    	git clone --recursive "$1" ~/.config/nvim/lua/config
+    fi
+fi
