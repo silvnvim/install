bdc577b751e3a3f0288b5b99c60151b490fe7bac
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 13:01:05 +0200

    apply rename changes

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.local/share/nvim ~/.local/share/nvim.bak
 
 # Clone core
-git clone https://github.com/silvnvim/nvim ~/.config/nvim
+git clone https://github.com/silvnvim/core ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 if [ $# -eq 0 ]
 then
-    git clone --recursive https://github.com/silvnvim/starter ~/.config/nvim/lua/config
+    git clone --recursive https://github.com/silvnvim/starter_config ~/.config/nvim/lua/config
 else
     if [ "$1" = "starter" ]
     then
-        git clone --recursive https://github.com/silvnvim/starter ~/.config/nvim/lua/config
+        git clone --recursive https://github.com/silvnvim/starter_config ~/.config/nvim/lua/config
     elif [ "$1" = "none" ]
     then
         :
